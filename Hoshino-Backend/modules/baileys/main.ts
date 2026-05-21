709a23238fb4d55f9a36db9a06097912bee5a315
XNS-ivy <ayramusic.nightcore@gmail.com>
2026-05-21 11:25:50 +0700

    update

--- Hoshino-Backend/modules/baileys/main.ts
+++ Hoshino-Backend/modules/baileys/main.ts
@@ -1,19 +1,37 @@
 import { makeWASocket, Browsers, fetchLatestWaWebVersion, useMultiFileAuthState } from 'baileys'
+import { ImprovedAuth } from './auth'
 import type { WASocket } from 'baileys'
 import { pino } from 'pino'
 import qrcode from 'qrcode-terminal'
 
 class BotManager {
     private sock: WASocket | null = null
+    private saveCreds: (() => Promise<void>) | null = null
     constructor() { }
     // create instance file
     async createBotInstance(user_id: string, phoneNumber: string | null) {
         try {
-            
+            const authFolder = `/auth/${user_id}/`
+            const useImprovedAuth = new ImprovedAuth(authFolder)
+            const { state, saveCreds } = useImprovedAuth
+            this.sock = makeWASocket({
+                auth: state,
+                browser: Browsers.appropriate("Google Chrome"),
+                version: (await fetchLatestWaWebVersion()).version,
+                logger: pino({ level: 'silent' }),
+            })
+            this.saveCreds = saveCreds
+            await this.startEvent()
         } catch (error) {
             console.error('Failed to start Whatsapp:', error)
             process.exit(1)
         }
+    }
+    private async startEvent(){
+        if (!this.sock) return
+        this.sock.ev.on('connection.update', (update) =>{
+            
+        })
     }
 }
 
@@ -22,16 +40,16 @@ export default Hoshino
 
 type BotStatus = 'idle' | 'connecting' | 'pairing' | 'connected' | 'disconnected'
 
- /**
-  * @interface BotInstance - this is bot controller interface
-  * @param BotInstance - the unique user id
-  * @param status - bot status from {@link BotStatus}
-  * @param PN - this is a phone number
-  * @param socket - {@link WASocket}
-  */
+/**
+ * @interface BotInstance - this is bot controller interface
+ * @param BotInstance - the unique user id
+ * @param status - bot status from {@link BotStatus}
+ * @param PN - this is a phone number
+ * @param socket - {@link WASocket}
+ */
 interface BotInstance {
     userID: string,
     status: BotStatus,
     PN: string | null,
-    socket: WASocket 
+    socket: WASocket
 }
